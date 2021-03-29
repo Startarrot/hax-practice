093f11285c77cb9b7b6e75a7ecd7b8f56299d654
Startarrot <robertchambers4444@gmail.com>
2021-03-29 09:00:16 -0500

    Hello world and Hero
    Hello world has a different font and hero is controlled by arrow keys.  Couldn't get color to change in hero class but did in playstate.  I have it set to toggle by a boolean if hello world or hero plays in at the top of playstate.

--- source/Hero.hx
+++ source/Hero.hx
@@ -1,13 +1,94 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+
 class Hero extends FlxSprite
 {
-	override public function new()
+	// How fast Hero moves
+	static inline var SPEED:Float = 50;
+
+	var hue:Float = 0;
+
+	// Create Hero
+	public function new(x:Float = 0, y:Float = 0)
 	{
-		super();
-		makeGraphic(10, 10, FlxColor = FlxColor.BLUE)
+		super(x, y);
+
+		makeGraphic(20, 20, FlxColor.WHITE);
+	}
+
+	// Gets user input to move
+	function updateMovement()
+	{
+		var up:Bool = false;
+		var down:Bool = false;
+		var left:Bool = false;
+		var right:Bool = false;
+
+		up = FlxG.keys.anyPressed([UP, W]);
+		down = FlxG.keys.anyPressed([DOWN, S]);
+		left = FlxG.keys.anyPressed([LEFT, A]);
+		right = FlxG.keys.anyPressed([RIGHT, D]);
+
+		if (up && down)
+		{
+			up = down = false;
+		}
+		if (left && right)
+		{
+			left = right = false;
+		}
+
+		if (up || down || left || right)
+		{
+			var newAngle:Float = 0;
+
+			if (up)
+			{
+				newAngle = -90;
+				if (left)
+				{
+					newAngle -= 45;
+				}
+				else if (right)
+				{
+					newAngle += 45;
+				}
+			}
+			else if (down)
+			{
+				newAngle = 90;
+
+				if (left)
+				{
+					newAngle += 45;
+				}
+				else if (right)
+				{
+					newAngle -= 45;
+				}
+			}
+			else if (left)
+			{
+				newAngle = 180;
+			}
+			else if (right)
+			{
+				newAngle = 0;
+			}
+
+			velocity.set(SPEED, 0);
+			velocity.rotate(FlxPoint.weak(0, 0), newAngle);
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
+		updateMovement();
+
 		super.update(elapsed);
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,18 +2,50 @@ package;
 
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var hero:Hero;
+	var hue:Float = 0;
+
+	// switch for Hello World or Hero
+	var hello:Bool = true;
+
 	override public function create()
 	{
-		super.create();
-
-		add(new FlxText(150, 200, 0, "Hello World!").setFormat("Times New Roman", 64));
+		if (hello == true)
+		{
+			// Create hello world
+			add(new FlxText(150, 200, 0, "Hello World!").setFormat("Times New Roman", 64));
+		}
+		else
+		{
+			// Create hero
+			hero = new Hero(200, 200);
+			add(hero);
+			super.create();
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
+		// Change hero's color
+		if (hello != true)
+		{
+			hue = hero.x + hero.y;
+			if (hue > 360)
+			{
+				hue -= 360;
+			}
+			if (hue < 0)
+			{
+				hue += 360;
+			}
+			var color = FlxColor.fromHSB(Std.int(hue), 1, 1);
+			hero.color = color;
+		}
+
 		super.update(elapsed);
 	}
 }
