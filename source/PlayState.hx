190e2ffba3bda4b3ae08c51800f7051ca1116c90
Startarrot <robertchambers4444@gmail.com>
2021-03-29 12:47:12 -0500

    minor fixes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,42 +9,30 @@ class PlayState extends FlxState
 	var hero:Hero;
 	var hue:Float = 0;
 
-	// switch for Hello World or Hero
-	var hello:Bool = true;
-
 	override public function create()
 	{
-		if (hello == true)
-		{
-			// Create hello world
-			add(new FlxText(150, 200, 0, "Hello World!").setFormat("Times New Roman", 64));
-		}
-		else
-		{
-			// Create hero
-			hero = new Hero(200, 200);
-			add(hero);
-			super.create();
-		}
+		// Create hello world
+		add(new FlxText(150, 200, 0, "Hello World!").setFormat("Times New Roman", 64));
+
+		// Create hero
+		hero = new Hero(200, 200);
+		add(hero);
+		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
-		// Change hero's color
-		if (hello != true)
+		hue = hero.x + hero.y;
+		if (hue > 360)
 		{
-			hue = hero.x + hero.y;
-			if (hue > 360)
-			{
-				hue -= 360;
-			}
-			if (hue < 0)
-			{
-				hue += 360;
-			}
-			var color = FlxColor.fromHSB(Std.int(hue), 1, 1);
-			hero.color = color;
+			hue -= 360;
 		}
+		if (hue < 0)
+		{
+			hue += 360;
+		}
+		var color = FlxColor.fromHSB(Std.int(hue), 1, 1);
+		hero.color = color;
 
 		super.update(elapsed);
 	}
